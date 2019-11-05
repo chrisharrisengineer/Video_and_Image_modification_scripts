eb1f0bd2681b138ccb7f4132cdb989bfd048517b
Chris Harris <chrisharrisglobal@gmail.com>
2019-11-05 15:20:22 -0800

    Delete image_to_jpg.py

--- image_to_jpg.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image  # Python Image Library - Image Processing
-import glob
-
-for file in glob.glob("*.jpg"):
-    im = Image.open(file)
-    rgb_im = im.convert('RGB')
-    rgb_im.save(file.replace("jpg", "jpg"), quality=95)
-
